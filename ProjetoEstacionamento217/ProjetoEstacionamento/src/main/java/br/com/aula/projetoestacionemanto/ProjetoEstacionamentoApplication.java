9f4e4c5b4cf18e17a57909c26be218f1a93ef689
susana <95098889+susana108@users.noreply.github.com>
2023-09-27 11:19:34 -0300

    projeto estacionamento
    projeto

---
+++ ProjetoEstacionamento217/ProjetoEstacionamento/src/main/java/br/com/aula/projetoestacionemanto/ProjetoEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula.projetoestacionemanto;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstacionamentoApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ProjetoEstacionamentoApplication.class, args);
+    }
+}
